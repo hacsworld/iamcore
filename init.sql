5ff134ed72e1a4733d6013089b04eafff90e03d9
hacsworld <givboxworld@gmail.com>
2025-09-14 10:43:20 +0700

    Delete init.sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS vector;
-
-CREATE TABLE IF NOT EXISTS items (
-    id SERIAL PRIMARY KEY,
-    text TEXT,
-    embedding VECTOR(384),
-    metadata JSONB DEFAULT '{}'
-);
